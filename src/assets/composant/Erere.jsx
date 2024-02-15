3633637ba1a09e4d176a9b6351601b101f671ee3
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-02-15 00:13:49 -0800

    back to home page

--- src/assets/composant/Erere.jsx
+++ src/assets/composant/Erere.jsx
@@ -5,9 +5,9 @@ function Erere() {
 
   return (
     <>
-      <p>it seem like this page doesn't exist try to go back to home page</p>
+      <p>It seem like this page doesn't exist try to go back to home page</p>
       <p>by using this link below</p>
-      <Link to="/">Go to home Page</Link>
+      <Link to="/">Back to home Page</Link>
     </>
   )
 }

c2d91fca6644da986608810c794a3bc4be5a5692
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-02-20 22:58:37 -0800

    latest

--- src/assets/composant/Skils.jsx
+++ src/assets/composant/Skils.jsx
@@ -8,7 +8,7 @@ function Skils() {
       <div className="skill_all_div">
       <h1>skills</h1>
         <p> pariatur dolor facere veritatis unde ipsa fugiat facilis laboriosam!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni modi alias, aspernatur aliquam animi nam perferendis. Libero aliquid deleniti quia quas velit dolorum, quisquam fugiat sed, unde alias ipsam fuga!</p>
+        <p>Lorem ipsum dolor sit,adipisicing elit. Magni modi alias, aspernatur aliquam animi nam perferendis. Libero aliquid deleniti quia quas velit dolorum, quisquam fugiat sed, unde alias ipsam fuga!</p>
         <nav>
         <h2>HTML5</h2>
         <h2>CSS</h2>
